439a3cb13c2743dcc1fc17bd84a2a828a4459a6b
David Doan <david@avvo.com>
2012-03-01 10:20:55 -0800

    fix cache lookups for belongs_to associations

--- lib/activerecord_cache/belongs_to_association.rb
+++ lib/activerecord_cache/belongs_to_association.rb
@@ -12,7 +12,7 @@ module ActiveRecordCache
     private
     
       def find_target_with_caching
-        if klass.use_activerecord_cache && reflection.foreign_key == klass.primary_key
+        if klass.use_activerecord_cache && reflection.options[:primary_key].nil?
           klass.find_through_cache(owner[reflection.foreign_key]).tap { |record| set_inverse_instance(record) }
         else
           find_target_without_caching
