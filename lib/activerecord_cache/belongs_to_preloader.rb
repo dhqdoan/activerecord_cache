9277bcea62f7c927c8d26826da4934895e88e650
Justin Weiss <justin@uberweiss.org>
2013-01-14 13:33:15 -0800

    Fix ActiveSupport::Concern InstanceMethods deprecation warnings

--- lib/activerecord_cache/belongs_to_preloader.rb
+++ lib/activerecord_cache/belongs_to_preloader.rb
@@ -1,23 +1,17 @@
 module ActiveRecordCache
   module BelongsToPreloader
     extend ActiveSupport::Concern
-    
+
     included do
       alias_method_chain :records_for, :caching
     end
-    
-    
-    module InstanceMethods
-      
-      def records_for_with_caching(ids)
-        if klass.use_activerecord_cache && association_key.name == klass.primary_key
-          klass.find_some_through_cache(ids)
-        else
-          records_for_without_caching(ids) 
-        end
+
+    def records_for_with_caching(ids)
+      if klass.use_activerecord_cache && association_key.name == klass.primary_key
+        klass.find_some_through_cache(ids)
+      else
+        records_for_without_caching(ids)
       end
-      
     end
-    
   end
 end
